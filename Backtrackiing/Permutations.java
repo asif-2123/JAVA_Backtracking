8f9ce07b7274ca0dd3dbe880a1a11534164638a4
Md Asif <asif212327@gmail.com>
2026-03-14 10:28:02 +0530

    Add files via upload

---
+++ Backtrackiing/Permutations.java
@@ -0,0 +1,21 @@
+public class Permutations {
+    public static void findPermutations(String str , String ans){
+        // Base Case
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        // Traverse through the string
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);                    // to get the current char
+            // To get the remaining char removing the current char
+            String newStr = str.substring(0,i) + str.substring(i+1); 
+            findPermutations(newStr,ans+curr);            //adding curr to ans and recursion
+        }
+    }    
+    public static void main(String args[]){
+        String str = "abc";
+        findPermutations(str,"");
+    }
+
+}
